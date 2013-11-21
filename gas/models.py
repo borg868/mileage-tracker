e5996e5a97e0cbd24c03e9f7f5b3bdecb90a7b34
calvin-n-hobbes <calvin-n-hobbes@bazinga.fake>
2013-11-20 21:45:11 -0800

    Edit gas model.

--- gas/models.py
+++ gas/models.py
@@ -5,7 +5,7 @@ class Fillup(models.Model):
     odometer = models.IntegerField()
     gallons = models.DecimalField(decimal_places=3, max_digits=5)
     price = models.DecimalField(name='Purchase Price', decimal_places=2, max_digits=5)
-    octane_rating = models.IntegerField()
+    octane_rating = models.IntegerField(max_digits=2)
     
     class Meta:
         db_table = 'fillups'
