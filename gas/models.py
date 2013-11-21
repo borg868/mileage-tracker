5668e46c2a0197c48951d58ddd547d2902b63ce9
郭漢傑 <kuoj@Rainbow-Road.local>
2013-11-20 20:44:22 -0800

    Created TrackMileage project.

---
+++ gas/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Fillup(models.Model):
+    mileage = models.DecimalField(decimal_places=1, max_digits=8)
+    purchase_date = models.DateField()
+    price = models.DecimalField(name='Purchase Price', decimal_places=2, max_digits=5)
+    octane_rating = models.IntegerField()
+    
+    class Meta:
+        db_table = 'fillups'
