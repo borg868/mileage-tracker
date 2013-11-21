931eb1da0a295bd2f1ebe71494553a69a570171d
borg868 <sybor.wang@gmail.com>
2013-11-20 21:26:43 -0800

    test

--- gas/models.py
+++ gas/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Fillup(models.Model):
-    mileage = models.DecimalField(decimal_places=1, max_digits=8)
+    odometer = models.DecimalField(decimal_places=1, max_digits=8)
     purchase_date = models.DateField()
     price = models.DecimalField(name='Purchase Price', decimal_places=2, max_digits=5)
     octane_rating = models.IntegerField()
