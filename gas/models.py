24f848ce91e1b02ae281b591dd38d3195d68bc37
borg868 <sybor.wang@gmail.com>
2013-11-20 21:31:20 -0800

    max digits

--- gas/models.py
+++ gas/models.py
@@ -4,7 +4,7 @@ class Fillup(models.Model):
     odometer = models.DecimalField(decimal_places=1, max_digits=8)
     purchase_date = models.DateField()
     price = models.DecimalField(name='Purchase Price', decimal_places=2, max_digits=5)
-    octane_rating = models.IntegerField()
+    octane_rating = models.IntegerField(max_digits=2)
     
     class Meta:
         db_table = 'fillups'
