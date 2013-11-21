d7ea4a5f0d68e388754d521f0a591c6b4aaec9df
calvin-n-hobbes <calvin-n-hobbes@bazinga.fake>
2013-11-20 22:09:14 -0800

    Fixed IntegerField.

--- gas/models.py
+++ gas/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Fillup(models.Model):
-    odometer = models.IntegerField
+    odometer = models.IntegerField()
     purchase_date = models.DateField()
     gallons = models.DecimalField(decimal_places=3, max_digits=5)
     price = models.DecimalField(name='Purchase Price', decimal_places=2, max_digits=5)
-    octane_rating = models.IntegerField(max_digits=2 )
+    octane_rating = models.IntegerField()
     
     class Meta:
         db_table = 'fillups'
