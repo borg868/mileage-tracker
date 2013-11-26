62b22e07bc985c92c0c43b164f37072bbb74ee32
calvin-n-hobbes <calvin-n-hobbes@bazinga.fake>
2013-11-26 01:22:13 -0800

    Added table 'users' to gas app.

--- gas/models.py
+++ gas/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
+class User(models.Model):
+    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=15)
+    
+    class Meta:
+        db_table = 'users'
+
 class Fillup(models.Model):
-    odometer = models.IntegerField()
+    user = models.ForeignKey('User')
     purchase_date = models.DateField()
+    odometer = models.IntegerField()
     gallons = models.DecimalField(decimal_places=3, max_digits=5)
     price = models.DecimalField(name='Purchase Price', decimal_places=2, max_digits=5)
     octane_rating = models.IntegerField()
