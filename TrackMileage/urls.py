eb3504fd0b1308e40979ce68d6c6822518859553
borg868 <sybor.wang@gmail.com>
2013-11-27 19:40:55 -0800

    views and urls

--- TrackMileage/urls.py
+++ TrackMileage/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
-
+from gas import views
 from django.contrib import admin
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +11,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^entry/',views.entry, name='entry')
 )
--- gas/views.py
+++ gas/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from gas.models import User, Fillup
 
 # Create your views here.
+def entry(request):
+    
+    context = {''}
+    return render(request, 'gas/entry.html', context)
