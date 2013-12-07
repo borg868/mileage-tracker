743a2e315fc67f85469fa9e28dfc9fb878fa5b5e
calvin-n-hobbes <calvin-n-hobbes@bazinga.fake>
2013-12-06 23:06:59 -0800

    Removed deprecated syntax.

--- TrackMileage/urls.py
+++ TrackMileage/urls.py
@@ -1,13 +1,8 @@
 from django.conf.urls import patterns, include, url
 from gas import views
 from django.contrib import admin
-from django.views.generic import list_detail
 from gas.models import Fillup
 from django.views.generic import ListView
-
-fillup_info = {
-    "queryset" : Fillup.objects.all(),
-}
 
 admin.autodiscover()
 
@@ -18,6 +13,5 @@ urlpatterns = patterns('',
 
    url(r'^admin/', include(admin.site.urls)),
    url(r'^entry/', views.entry, name='entry'),
-   url(r'^listing/', ListView.as_view(
-                                      model=Fillup), name='list')
+   url(r'^listing/', ListView.as_view(model=Fillup), name='list')
 )
